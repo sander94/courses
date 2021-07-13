f75f2b6636c7a293bc4c8add3a41215393ef6dbf
sander94 <sanderjyrgens@gmail.com>
2021-07-14 00:28:20 +0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@koolitused.ee'),
+        'name' => env('MAIL_FROM_NAME', 'Koolitused.ee'),
     ],
 
     /*
