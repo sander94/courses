d22d2f99a925842f60454032e9606dba967015ff
sander94 <sanderjyrgens@gmail.com>
2021-07-15 18:05:02 +0300

    Update properties.blade.php

--- resources/views/partials/search/properties.blade.php
+++ resources/views/partials/search/properties.blade.php
@@ -111,19 +111,24 @@
                 padding: 5px 10px;
             }
         </style>
-<div id="list">
-        @foreach($result as $property)
-                    @php
-                        $media = $property->getMedia('gallery');
-                        $urls = $media->map->getUrl();
-                    @endphp
+
+        <div class="row mt-3" id="list" style="text-align: left;">
+
+            <div class="col-12">
+
+                <!-- result element -->
+
+                @foreach($properties as $property)
+
                     @php
                         $servicearray = $property->services->implode('title',',');
                         $servicearray = strtolower($servicearray);
                         $servicearray = str_replace(',', ', ', $servicearray);
                     @endphp
+
                     <div class="row mt-5">
-                        <div class="col-3">
+                        <div class="col-12 col-md-3">
+                            <h3 class="text-orange hidedesktop">{{ $property->name }}</h3>
                             <div class="room-image"
                                  style="background-image: url('{{ $property->getFirstMediaUrl('cover') }}');">
                             </div>
@@ -131,17 +136,18 @@
                             @php
                                 $media = $property->getMedia('gallery');
                                 $urls = $media->map->getUrl();
+                                $thumbUrls = $media->map->getUrl('galleryThumb');
                             @endphp
 
-                            <div style="display: grid; grid-template-columns: repeat(3, minmax(0, 1fr)); column-gap: 5px; row-gap: 5px;">
-                            @foreach($urls as $index => $url)
+                            <div class="smallgallery">
+                            @foreach($thumbUrls as $index => $url)
                                 <img @click="showImg({{ json_encode($urls) }}, {!! $index !!})" class="galleryboxImg" src="{{ $url }}">
                             @endforeach
                             </div>
                         </div>
-                        <div class="col-9">
-                            <h3 class="text-orange">{{ $property->name }}</h3>
-                            <p><i class="fa fa-home fa-fw"> </i> {{ $property->address }}<br>
+                        <div class="col-12 col-md-9">
+                            <h3 class="text-orange hidemobile">{{ $property->name }}</h3>
+                            <p class="property-data"><i class="fa fa-home fa-fw"> </i> {{ $property->address }}<br>
                                 <i class="fa fa-briefcase fa-fw"> </i> {{ $property->company_name }}<br>
                                 @if($property->email) <i class="fa fa-envelope fa-fw"> </i> {{ $property->email }}<br> @endif
                                 @if($property->phone) <i class="fa fa-phone fa-fw fa-flip-horizontal"> </i> {{ $property->phone }}<br> @endif
@@ -151,9 +157,10 @@
                                 @if($servicearray != "")
                                     <br><strong>Teenused: </strong> {{ $servicearray }}
                                 @endif</p>
+                            @if($property->rooms->count() > 0)
                             <table class="roomstable">
                                 <tr style="background-color: #FFFFFF; height: 40px">
-                                    <td>Ruumi nimetus</td>
+                                    <td class="room_name">Ruum</td>
                                     <td class="text-center">m2</td>
                                     <td class="text-center"><img src="{{ asset('images/teater.png') }}"></td>
                                     <td class="text-center"><img src="{{ asset('images/klass.png') }}"></td>
@@ -164,7 +171,7 @@
                                 </tr>
                                 @foreach($property->rooms as $room)
                                     <tr style="height: 40px;">
-                                        <td>{{ $room->name }}</td>
+                                        <td class="room_name">{{ $room->name }}</td>
                                         <td class="text-center">{{ $room->square_meters }}</td>
                                         <td class="text-center"><i
                                                 class="fa fa-user"></i> {{ $room->theatre_style_capacity ?? 0 }}</td>
@@ -182,17 +189,23 @@
                                     </tr>
                                 @endforeach
                             </table>
+                            @endif
                         </div>
                     </div>
-        
-        @endforeach
-                        <vue-easy-lightbox
+                @endforeach
+
+                <vue-easy-lightbox
                     :visible="visible"
                     :imgs="imgs"
                     :index="index"
                     @hide="handleHide"
                 ></vue-easy-lightbox>
-</div>
+                <!-- result element end -->
+
+
+            </div>
+
+        </div>
 
 
     </div>
