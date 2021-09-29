5a59b7fef170c3658a1b85f4c609e87f4f4fc5fb
oleghalin <oleghalinua@gmail.com>
2021-09-29 11:02:02 +0300

    Fix company page

--- resources/views/companies/single.blade.php
+++ resources/views/companies/single.blade.php
@@ -49,9 +49,9 @@
             <div class="button-container">
                 @foreach($types as $type)
 
-                    <a href="?course_type={{ $type->getKey() }}#calendar"
-                       class="{{ $selectedType == $type->getKey() ? 'active' : null }}">{{ $type->title }}
-                        ({{ $counters[$type->getKey()] }})</a>
+                    <a href="?type={{ $type->getKey() }}#calendar"
+                       class="{{ $selectedCourseType == $type->getKey() ? 'active' : null }}">{{ $type->title }}
+                        ({{ $counts[$type->getKey()] }})</a>
                 @endforeach
             </div>
             <div class="results-table-container">
