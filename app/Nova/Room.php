6f4e678f28ca06f07242bf6fbba977b27da89e85
sander94 <sanderjyrgens@gmail.com>
2021-09-30 21:07:55 +0300

    Update Room.php

--- app/Nova/Room.php
+++ app/Nova/Room.php
@@ -73,7 +73,7 @@ class Room extends Resource
             Text::make('Cabaret style capacity')
                 ->rules('nullable', 'string'),
 
-            Link::make('Url'),
+            Link::make('Url')->rules('url'),
 
             BelongsTo::make('Property'),
         ];
