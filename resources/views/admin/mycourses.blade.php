d462a8b65bbf233222b810fc369a9ae392e4eb73
sander94 <sanderjyrgens@gmail.com>
2021-10-31 20:14:40 +0200

    courses

--- resources/views/admin/mycourses.blade.php
+++ resources/views/admin/mycourses.blade.php
@@ -131,7 +131,7 @@
                 </table>
 
                 <div class="pagination">
-                    {{ $courses->links() }}
+                    {{ $courses->appends(request()->all())->links() }}
                 </div>
 
             </div>
--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -425,6 +425,7 @@ figure img {
             width: 100%;
             height: 200px;
             background-size: cover;
+            background-position: center center;
         }
 
         .company-image-container {
