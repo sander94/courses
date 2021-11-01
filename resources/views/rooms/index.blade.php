7d766a4e822e65eaa11cbe6221e86405140852cb
sander94 <sanderjyrgens@gmail.com>
2021-11-01 18:45:38 +0200

    Update index.blade.php

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -16,8 +16,8 @@
             </div>
             @if($banner)
                 <div class="col-sm-8">
-                    <a href="{{ route('ad', $banner) }}">
-                        <img src="{{ $banner->getFirstMediaUrl('banner') }}" class="advert" target="_blank">
+                    <a href="{{ route('ad', $banner) }}" target="_blank">
+                        <img src="{{ $banner->getFirstMediaUrl('banner') }}" class="advert">
                     </a>
                 </div>
             @endif
