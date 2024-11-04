8fdaa8faf17dbb08b5e7125def3bd627272b65f1
sander94 <sanderjyrgens@gmail.com>
2024-11-05 00:32:07 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::resource('artiklid', 'ArticleController');
+Route::resource('articles', 'ArticleController');
 
 Route::resource('events', 'EventController');
 
