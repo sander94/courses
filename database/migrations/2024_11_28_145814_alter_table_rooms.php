6d1288aebe7358423e301338e379845bae84b615
sander94 <sanderjyrgens@gmail.com>
2024-11-28 17:01:17 +0200

    Create 2024_11_28_145814_alter_table_rooms.php

---
+++ database/migrations/2024_11_28_145814_alter_table_rooms.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableRooms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->string('square_meters')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->string('square_meters');
+        }
+    }
+};
